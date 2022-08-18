0f6ad70fd57d71ab900d699e84be9f6ad9c7e44e
thetalemon <sasami0314@gmail.com>
2022-08-18 23:14:00 +0900

    update: update ripple

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import p5 from 'p5';
 
-const itemList = [];
+function randBoolean() {
+  return Math.random() < 0.05;
+}
+
+type Ripple = {
+  center: { x: number; y: number };
+  diameterList: number[];
+};
+
+const rippleList: Ripple[] = [];
 
 const sketch = (p: p5) => {
   p.setup = () => {
@@ -17,19 +26,32 @@ const sketch = (p: p5) => {
       p.noFill();
       p.strokeWeight(1);
       p.stroke(44, 115, 255, 100);
-      itemList.forEach((item, index) => {
-        p.ellipse(item.x, item.y, item.diameter);
-        itemList[index] = {
-          x: item.x,
-          y: item.y,
-          diameter: item.diameter + 2,
-        };
+      rippleList.forEach((item, rippleIndex) => {
+        if (item.diameterList.length === 0) {
+          rippleList.splice(rippleIndex, 1);
+          return;
+        }
+
+        item.diameterList.forEach((diameter, diameterIndex) => {
+          if (Math.max(...item.diameterList) > 600) {
+            rippleList[rippleIndex].diameterList.splice(diameterIndex, 1);
+            return;
+          }
+          p.ellipse(item.center.x, item.center.y, diameter);
+          rippleList[rippleIndex].diameterList[diameterIndex] = diameter + 5;
+          if (0 === diameterIndex && randBoolean()) {
+            rippleList[rippleIndex].diameterList.unshift(diameter - 30);
+          }
+        });
       });
     }
   };
 
   p.mouseClicked = () => {
-    itemList.push({ x: p.mouseX, y: p.mouseY, diameter: 20 });
+    rippleList.push({
+      center: { x: p.mouseX, y: p.mouseY },
+      diameterList: [20],
+    });
   };
 };
 
